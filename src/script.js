fba292208a0e3b2094b1a72ffe1f2ed8ccc5f982
AyzansAlt <36968271+AyzansAlt@users.noreply.github.com>
2020-08-03 20:26:11 +0100

    Profi Emvbed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
+function myFunction() {
+var test = document.getElementById("search").value;
+document.getElementById("profi-embed").src =
+"https://locness3.github.io/scratch-profile-embed/#"+ test;
+}
 
