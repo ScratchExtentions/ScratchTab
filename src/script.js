61245618ee4a2b97c0cc9986bd831da86737c75e
AyzansAlt <36968271+AyzansAlt@users.noreply.github.com>
2020-08-03 21:44:08 +0100

    Saving Data

--- src/script.js
+++ src/script.js
@@ -1,6 +1,20 @@
+
+getProfiKey = localStorage.getItem('101')
+console.log(getProfiKey)
+if (getProfiKey == "null"){
+var test = document.getElementById("search").value;
+document.getElementById("profi-embed").src =
+"https://locness3.github.io/scratch-profile-embed/#griffpatch";
+} else {
+    var test = document.getElementById("search").value;
+document.getElementById("profi-embed").src =
+"https://locness3.github.io/scratch-profile-embed/#"+ getProfiKey;
+}
+
 function myFunction() {
 var test = document.getElementById("search").value;
 document.getElementById("profi-embed").src =
 "https://locness3.github.io/scratch-profile-embed/#"+ test;
+localStorage.setItem('101', test)
 }
 
